56d30dd5904a9c0b0bb27c356e4a484e6913590c
Vitor Candido <vitor.candido900@gmail.com>
2025-02-24 15:16:33 -0300

    Escolha aleatoria

--- js/app.js
+++ js/app.js
@@ -18,8 +18,6 @@ function adicionar() {
     nomeInserido.value = ''; // Limpa o campo de entrada
 }
 
-
-
 function sortear(){ 
     // Verifica se há pelo menos 3 amigos para o sorteio
     if (amigos.length < 3) {
@@ -27,28 +25,26 @@ function sortear(){
         return; // Impede o sorteio se houver menos de 3 amigos
     }
 
-    embaralha(amigos); // Embaralha os amigos
+    embaralha(amigos); // Embaralha os amigos aleatoriamente
     let sorteio = document.getElementById("lista-sorteio");
 
-    // Realiza o sorteio e exibe o resultado
+    // Exibe o sorteio
     for(let i = 0; i < amigos.length; i++){ 
         
         if(i == amigos.length - 1){
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>'; // Conecta o último amigo com o primeiro
         } else{
-            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>'; // Conecta amigos consecutivos
         }
     }
 }
 
-
 function embaralha(amigos) { // Para embaralhar o array
 
     for (let indice = amigos.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice); // Gera um índice aleatório
 
-        const indiceAleatorio = Math.floor(Math.random() * indice);
-
-        // atribuição via destructuring
+        // Atribuição via destructuring para trocar os elementos de posição
         [amigos[indice - 1], amigos[indiceAleatorio]] = 
             [amigos[indiceAleatorio], amigos[indice - 1]];
     }
@@ -58,5 +54,4 @@ function reiniciar(){
     amigos = [];
     document.getElementById("lista-amigos").innerHTML = '';
     let sorteio = document.getElementById("lista-sorteio").innerHTML = '';
-
 }
